fd159ff923e56eeafb9e46b4f3d236910c1b1ea2
Devika18 <devikankarpe@gmail.com>
2022-11-22 09:44:13 +0530

    added: logic for creating new contact

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,5 +1,14 @@
 public class AddressBookMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to AddressBook Stream Program");
+        System.out.println("Welcome to Address Book");
+        Contacts personDetails=new Contacts();
+        personDetails.setFirstName("Vivek");
+        personDetails.setLastName("Gujale");
+        personDetails.setAddress("Kalamboli");
+        personDetails.setCity("Panvel");
+        personDetails.setState("Maharashtra");
+        personDetails.setZipCode(456123);
+        personDetails.setPhoneNumber(1234567890L);
+        System.out.println(personDetails);
     }
 }
